f8d251b1b3dd22d85a98185f841ac3f5cbf2ff57
nikolaipatrick <nikolaipatricknp@gmail.com>
2022-05-31 10:50:12 +0930

    initialize the display

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,30 @@
 #include <Arduino.h>
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include <Adafruit_I2CDevice.h>
+
+//Pre-processor variables
+#define SCREEN_ADDRESS 0x3C;
+#define SCREEN_WIDTH 128;
+#define SCREEN_HEIGHT 64;
+#define OLED_RESET     4 ;
+#define BUTTON_1 8;
+#define BUTTON_2 7;
+#define BUTTON_3 6;
+
+//Initialize the display:
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 void setup() {
-  // put your setup code here, to run once:
+
+  if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;);
+  }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
 }
